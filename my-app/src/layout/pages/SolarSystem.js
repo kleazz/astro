3b88a99ee445037cb79fa1b0ccc4d9689a56f1a2
Rita Mala <ritamalaa.1@gmail.com>
2024-05-29 21:09:20 +0200

    Added info boxes to the solar system page

--- my-app/src/layout/pages/SolarSystem.js
+++ my-app/src/layout/pages/SolarSystem.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import '../style/solarsystem.css';
 import sun from '../../sources/images/planets/sun.png';
-// import PlanetFact from '../components/PlanetFact';
 
 const SolarSystem = () => {
   useEffect(() => {
@@ -10,7 +9,7 @@ const SolarSystem = () => {
 
   const createStars = () => {
     const container = document.querySelector(".solar-system-container");
-    for (let i = 0; i < 500; i++) {
+    for (let i = 0; i < 750; i++) {
       const star = document.createElement("div");
       star.className = "star";
       star.style.width = ".1px";
@@ -23,28 +22,37 @@ const SolarSystem = () => {
 
   return (
     <>
-    <div className="solar-system-container planets">
-      <div className="sun">
-        <img src={sun} alt="sun" />
+      <div className="info-box top-left">
+        <h3>Left Top</h3>
+        <p>This is a box on the top-left side.</p>
       </div>
-      <div className="mercury"></div>
-      <div className="venus"></div>
-      <div className="earth">
-        <div className="moon"></div>
+      <div className="info-text bottom-left">
+        <p>This is just text on the bottom-left side.</p>
       </div>
-      <div className="mars"></div>
-      <div className="jupiter"></div>
-      <div className="saturn"></div>
-      <div className="uranus"></div>
-      <div className="neptune"></div>
-      <div className="pluto"></div>
-    </div>
-    {/* <PlanetFact
-        imageUrl={sun}
-        title="Planet Name"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-      />     */}
-      </>
+      <div className="info-text top-right">
+        <p>This is just text on the top-right side.</p>
+      </div>
+      <div className="info-box bottom-right">
+        <h3>Right Bottom</h3>
+        <p>This is a box on the top-left side.</p>
+      </div>
+      <div className="solar-system-container planets">
+        <div className="sun">
+          <img src={sun} alt="sun" />
+        </div>
+        <div className="mercury"></div>
+        <div className="venus"></div>
+        <div className="earth">
+          <div className="moon"></div>
+        </div>
+        <div className="mars"></div>
+        <div className="jupiter"></div>
+        <div className="saturn"></div>
+        <div className="uranus"></div>
+        <div className="neptune"></div>
+        <div className="pluto"></div>
+      </div>
+    </>
   );
 };
 
